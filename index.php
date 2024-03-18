501241b794a7d280c9320eaeed9342f90fd95847
asep38 <asepzarkasihnoor38@gmail.com>
2024-03-18 11:02:35 +0700

    feat: perubahan struktur

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php require "includes/header.php"; ?>
+
+<body class="sb-nav-fixed">
+    <?php require "includes/navbar.php"; ?>
+    <div id="layoutSidenav">
+        <div id="layoutSidenav_nav">
+            <?php require "includes/sidebar.php"; ?>
+        </div>
+    </div>
+    <div id="layoutSidenav_content"></div>
+    <?php require "includes/footer.php"; ?>
+    <?php require "includes/js.php"; ?>
+</body>
+
+</html>
